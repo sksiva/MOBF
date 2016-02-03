c572dea8570c56138d34192e7d7d4875da18dd9d
Sivakumar N <sivakumar.n@skavacbe.local>
2016-02-03 16:13:32 +0530

    Complete Freeze for Demo 1

--- src/pak/MOBFArchiveOrder.java
+++ src/pak/MOBFArchiveOrder.java
@@ -1,4 +1,4 @@
-package Test;
+package pak;
 
 import java.awt.Robot;
 import java.awt.event.InputEvent;
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-public class MOBFArchieveOrder extends Attributes{
+public class MOBFArchiveOrder extends Attributes{
 	  @Test
  public void REVERSESWIPE() throws Exception {
 		  
